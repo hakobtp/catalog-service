4de2116e7c678d0aef6d742b3765255bdbc4a4e0
Hakob Ter-Petrosyan <hakob.ter-petrosyan@dataart.com>
2023-02-16 11:44:09 +0400

    unit test

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
+//		http POST :9001/books author="Jon Snow" title="" isbn="123ABC456Z" price=9.90
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 
--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -3,18 +3,20 @@ package com.polarbookshop.catalogservice.domain;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.NotNull;
 
 public record Book(
-        @NotBlank(message = "The book ISBN must be defined.") 
+        @NotBlank(message = "The book ISBN must be defined.")
         @Pattern(
-                regexp = "^([0-9]{10}|[0-9]{13})$", 
+                regexp = "^([0-9]{10}|[0-9]{13})$",
                 message = "The ISBN format must be valid."
-        ) 
+        )
         String isbn,
         @NotBlank(message = "The book title must be defined.")
         String title,
         @NotBlank(message = "The book author must be defined.")
         String author,
+        @NotNull(message = "The book price must be defined.")
         @Positive(message = "The book price must be greater than zero.")
         Double price) {
 }
